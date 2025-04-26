7f94e793e81a2355b0c1a9a800a2cc57b2fcf1e1
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-04-26 19:04:28 +0200

    Added file for challenge 2.

--- src/challenge-1.jl
+++ src/challenge-1.jl
@@ -1,9 +1,11 @@
 #=
-Functions for converting bit (or rather byte) streams to Base64 and converting Base64 to the original stream.
+Functions for converting bit (or rather byte) streams to Base64, and converting Base64 to the original stream.
 =#
 
-function base64_encode()
+# Base64 encoding with padding.
+function base64_encode(bytes::Vector{UInt8})::String
 end
 
-function base64_decode()
+# Decoding of Base64 with padding.
+function base64_decode(base64::AbstractString)::Vector{UInt8}
 end
