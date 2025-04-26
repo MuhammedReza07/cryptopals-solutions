8124884d339a5ec76c3cf2369d9902d6f4ed2323
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-04-26 18:33:39 +0200

    Added boilerplate for challenge 1.

---
+++ src/challenge-1.jl
@@ -0,0 +1,9 @@
+#=
+Functions for converting bit (or rather byte) streams to Base64 and converting Base64 to the original stream.
+=#
+
+function base64_encode()
+end
+
+function base64_decode()
+end
