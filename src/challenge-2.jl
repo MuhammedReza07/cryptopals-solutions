ac7f79e346177a55356b2b8aa5e3f5755835f949
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-04-27 15:49:29 +0200

    Finished solution to challenge 2.

---
+++ src/challenge-2.jl
@@ -0,0 +1,13 @@
+#=
+Function that produces the XOR combination of two buffers of equal length.
+=#
+
+function xor_buffers(a::Vector{UInt8}, b::Vector{UInt8})::Vector{UInt8}
+    if length(a) != length(b)
+        ArgumentError("buffers must be of equal length") |> throw
+    end
+
+    xor.(a, b)
+end
+
+xor_buffers_hex(a::AbstractString, b::AbstractString)::String = xor_buffers(hex2bytes(a), hex2bytes(b)) |> bytes2hex
